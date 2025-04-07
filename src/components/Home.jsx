8e9d141297af954f67b731b5b6cb53c89457683f
Mauli Kashid <kashidnamdevmauli777@gmail.com>
2025-04-07 19:09:25 +0530

    change create task

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <main className='p-10 md:w-[70%] h-full '>
             <h1 className='md:text-5xl text-3xl my-10'>Make good communication with your employees</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit explicabo rerum earum neque consequuntur excepturi, aperiam autem doloribus voluptates labore magnam delectus fuga quisquam sapiente ipsum repudiandae nisi aspernatur ipsa odio reprehenderit accusamus? Repellat dolore, odit, odio ex unde nisi dicta expedita necessitatibus alias, fugit harum nostrum assumenda porro accusantium!</p>
+            <p>An Employee Management System (EMS) is a comprehensive software application designed to streamline the human resource functions of an organization. It enables businesses to efficiently manage employee information, monitor performance, track attendance, process payroll, and handle other administrative tasks. By digitizing and automating routine HR activities, EMS reduces manual errors, saves time, and enhances organizational productivity.</p>
             <a className=' bg-green-600 my-5 text-lg w-20 text-center font-bold inline-block rounded-lg p-2 ' href="/login">Login</a>
         </main>
     )
